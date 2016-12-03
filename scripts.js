8cba6b3261702daf04ce86bf64ccb134ad8a09d9
MALEK <MALEK.KANAAN@OUTLOOK.COM>
2016-12-03 18:05:22 +0100

    some styles and it show now more info

--- scripts.js
+++ scripts.js
@@ -2,25 +2,23 @@ var userName;
 
 var data;
 
-
-
 function getAll(){
 $.ajax({
  method: "GET",
  url: "https://api.github.com/users/" + userName,
 })
- .done(function( response) {
+ .done(function(response) {
   	data = response;
   	console.log(data.avatar_url);
-  	var image = "<img src='" + data.avatar_url + "'" + "width=" + "250px" + "height=" + "250px" + "img>";
-	var link = "<a target='_blank' class='result-link' href='" + data.html_url + "'" + ">GitHub</a>";
-  $("#profile-image").append(image,link);
+  	
+  	var image = "<p>hello " + data.name + "!!</p><a target='_blank' href='" + data.html_url + "'" + "><img src='" + data.avatar_url + "'" + "width=" + "250px" + "height=" + "250px" + "img></a>";
+	var folowers = "<p>you follow " + data.following +" users and you have "+data.followers+" users interested in your "+ data.public_repos + " repositories</p><p>you'r located in " + data.location +"</p>"
+  $("#profile").empty().append(name,image,folowers);
+  	console.log(data);
  });
 }
 function getUserName() {
   userName = document.getElementById("userInput").value;
- //console.log(userName);
- getAll();
- 
+   getAll();
 }
 

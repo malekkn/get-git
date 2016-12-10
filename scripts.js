6993808502fedddd15b032609f9f517d47aed5cf
MALEK <MALEK.KANAAN@OUTLOOK.COM>
2016-12-10 17:51:44 +0100

    indentation and comments

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 
 var data;
 // ----------------------------request functions----------------------------
-
 function getAll(){
 	$.ajax({
 	 method: "GET",
@@ -9,24 +8,7 @@ function getAll(){
 	})
 	.done(function(response) {
 	  	data = response;
-	  	  	
-	  	var image = `<p>hello ${data.name} !!</p>
-					<a target='_blank' href='${data.html_url}'>
-					<img src='${data.avatar_url}' img>
-					</a>`;
-		var info = `
-			<p>you'r located in ${data.location} </p>
-			<p>you follow ${data.following} users and you have ${data.followers} users interested in your ${data.public_repos}  
-			<a href="#" onclick="writeNames()" title="list of the repositories.">repositories</p></a>
-			<ul id="list"></ul>`;
-		var altImage = `<p>If u find your profile picture a bit boring<br>check this unique adorable one out!!</p>
-						<a target='_blank' href='https://api.adorable.io/avatars/200/${userName}.png'>
-						<img src='https://api.adorable.io/avatars/200/${userName}.png' img>
-						</a>`;
-		hideElement('loader','wrapper');
-		$("#profile").empty().append(image,info,altImage);
-	  	getRepos();
-	  	console.log(data);
+	  	populatingDOM();
 	});
 }
 
@@ -37,26 +19,46 @@ function getRepos(){
 	})
 	.done(function(repositories) {
 		data.namesOfRrpositories = repositories;
-		console.log(data.namesOfRrpositories[0].name);
 	});
 }
-// --------------------------------------------------------
+// --------------------------populating functions------------------------------
+function populatingDOM () {
+
+  	var image = `
+  		<p>hello ${data.name} !!</p>
+		<a target='_blank' href='${data.html_url}'>
+		<img src='${data.avatar_url}' img></a>`;
+	var info = `
+		<p>you'r located in ${data.location} </p>
+		<p>you follow ${data.following} users and you have ${data.followers} users interested in your ${data.public_repos}  
+		<a href="#" onclick="writeNames()" title="list of the repositories.">repositories</p></a>
+		<ul id="list"></ul>`;
+	var altImage = `
+		<p>If u find your profile picture a bit boring<br>check this unique adorable one out!!</p>
+		<a target='_blank' href='https://api.adorable.io/avatars/200/${userName}.png'>
+		<img src='https://api.adorable.io/avatars/200/${userName}.png' img>
+		</a>`;
+	hideElement('loader','wrapper');
+	$("#profile").empty().append(image,info,altImage);
+  	getRepos();
+  	console.log(data);
+}
 
 function writeNames(){
-	$("#list").append("<p>wich are :</p>")
+	$("#list").empty().append("<p>wich are :</p>");
 	for (var i = 0; i < data.public_repos; i++) {
 		var listEl = `<li>${data.namesOfRrpositories[i].name}</li>`
 		$("#list").append(listEl);
-		console.log(i);
+		console.log(data.namesOfRrpositories[i].name);
 	}
 }
-
+//------------------------------gets the user input ------------------------------
 function getUserName() {
   userName = document.getElementById("userInput").value;
    getAll();
    showElement('loader','wrapper');
 }
-
+//----------------------------loader functions----------------------------
 function hideElement (x,y) {
 	document.getElementById(x).style.display = "none";
 	document.getElementById(y).style.height = "auto";
